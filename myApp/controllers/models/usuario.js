1dbc3182cfa908d0d72836d0650ed36fa2c4dde0
marucalliera <89428723+marucalliera@users.noreply.github.com>
2022-04-16 04:05:44 -0300

    Modificando sin romper el codigo

---
+++ myApp/controllers/models/usuario.js
@@ -0,0 +1,9 @@
+//Objeto lit se completa con los datos con los que llene el usuario
+
+let usuario ={
+    mail: "juancitoperez@gmail.com",
+    nombre: "juan",
+    apellido: "perez",
+    edad: "22",
+};
+module.exports = usuario
--- myApp/routes/users.js
+++ myApp/routes/users.js
@@ -1,3 +1,4 @@
+//Importamos cheeck
 const express = require('express');
 const router = express.Router();
 
