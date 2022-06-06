b2efe589634c12340bf7f0f22aab74cf18aff76d
BrunogGallo <brunogiorgiogallo@mail.com>
2022-06-06 10:06:11 -0300

    emprolijo tablas

--- myApp/db/data.sql
+++ myApp/db/data.sql
@@ -3,24 +3,21 @@ CREATE SCHEMA localHost
 CREATE TABLE  usuario (
 /*Nombre  columna   tipoDato  Restricciones*/
 id INT UNSIGNED PRIMARY KEY  AUTO_INCREMENT, 
-/*lA COMA ES PARA NA SEGUNDA COLUMNA, AQUI SEGUNDA COLUMNA*/
-nombre      VARCHAR(50)        NOT NULL,
-apellido    VARCHAR(50)      NOT NULL,
-email       VARCHAR(50)      NOT NULL,
-nombreUsuario     VARCHAR(50)      NOT NULL,
-contraseña       VARCHAR(50)      NOT NULL,
-fechaNacimiento       DATE      NOT NULL,
-nroDocumento      INT      NOT NULL,
-/*aQUI EN FOTO PONEMRO VARCHAR POR Q SE GUARDA COMO STRING*/
-fotoPerfil          VARCHAR(360)
+nombre          VARCHAR(50)      NOT NULL,
+apellido        VARCHAR(50)      NOT NULL,
+email           VARCHAR(50)      NOT NULL,
+nombreUsuario   VARCHAR(50)      NOT NULL,
+contraseña      VARCHAR(50)      NOT NULL,
+fechaNacimiento DATE             NOT NULL,
+nroDocumento    INT              NOT NULL,
+fotoPerfil      VARCHAR(360)
 );
 
 CREATE TABLE  comentarios (
 
-id INT UNSIGNED PRIMARY KEY  AUTO_INCREMENT, 
-
+id INT UNSIGNED    PRIMARY KEY  AUTO_INCREMENT, 
 nombreUsuario      VARCHAR(50)        NOT NULL,
-text     VARCHAR(500)      NOT NULL,
-contraseña       VARCHAR(50)      NOT NULL,
-imagen          VARCHAR(360)
+text               VARCHAR(500)      NOT NULL,
+contraseña         VARCHAR(50)      NOT NULL,
+imagen             VARCHAR(360)
 );
