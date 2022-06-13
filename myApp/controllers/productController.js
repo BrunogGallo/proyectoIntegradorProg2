0c1045a923d328bc2b002046d11583de74488eb4
BrunogGallo <brunogiorgiogallo@mail.com>
2022-06-13 10:40:04 -0300

    cambios controladores

--- myApp/controllers/productController.js
+++ myApp/controllers/productController.js
@@ -1,9 +1,38 @@
-const db = require('../db/data')
-const user = db.user
-const productos = db.productos
-const comentarios = db.comentarios
+const db = require('../database/models')
+
+const users = db.User
+const productos = db.Producto
+const comentarios = db.Comentario
 
 const productoController = {
+
+    showOne: function (req, res) { //Este es mostrar producto, detallesProducto
+        let busqueda = req.query.search
+        productos.findAll({
+            where: [{nombreProducto: busqueda}]
+        })
+        then ((result) => {
+            let fi1 = new Date(result.createdAt);
+            fechaPost = `${fi1.getDate()}-${fi1.getMonth() + 1}-${fi1.getFullYear()}`;
+
+            let fi2 = new Date(result.updatedAt);
+            fechaEdit = `${fi2.getDate()}-${fi2.getMonth() + 1}-${fi2.getFullYear()}`;
+
+            let producto = {
+                nombreProducto: result.nombreProducto,
+                descripcion: result.descripcion,
+                imagen: result.imagen,
+                comentarios: result.idComentario,
+                fechaPosteo: fechaPost,
+                fechaEdicion: fechaEdit
+            }
+
+            return res.render ('products', {
+                productos: producto,
+                comentarios: comentarios
+            })
+        })
+    },
     mostrarProducto: function (req, res) {
         return res.render ('products', {
             'productos': productos[0],
