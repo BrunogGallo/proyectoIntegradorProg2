2ffde1c8a372d4ff2b3e75b78b8976c69f8cfde0
marucalliera <89428723+marucalliera@users.noreply.github.com>
2022-06-16 12:11:11 -0300

    palabras para guiarme

--- myApp/controllers/userController.js
+++ myApp/controllers/userController.js
@@ -17,16 +17,22 @@ const userController = {
         user.findOne({
             where : [{ email :  info.email}]
         }).then((result) => {
+            //El result de abajo me trae la iunformacion del usuario
+            //Lo que se ejecuta es lo siguiente--> me trjo la informacion, info que fue la informacion  q yo envie en el formulario, busco uno, me trae un result, ese result
+            //es la informarcion dlel usuario de la base de datos, ese result es un obj literal con todas las formas.  Nosotros queremos guaradar ese database .
+            //Si clave correcta es verdadero va a entrar si existe un mai, eso es un paso a pasao de lo que hace, 
+            //Dentro de clave correcya utilizo req. resiion .user y le pomgo esa prop
             if (result != null) {
                 let claveCorrecta = bcryptjs.compareSync(info.password  , result.password )
                 if (claveCorrecta) {
+
                     return res.send("Existe el mail " + result.email + " y la clave es correcta")
                 } else {
                     return res.send("Existe el mail " + result.email + " pero la clave es incorrecta")
                 }
 
             } else {
-                return res.send("No xiste el mail " + info.email) 
+                return res.send("No existe el mail " + info.email) 
             }
         });
     },
