2f629f24ba970ea287b3b7b9deb55af90e415128
marucalliera <89428723+marucalliera@users.noreply.github.com>
2022-06-16 11:25:12 -0300

    Cambios en modelos sigo trabajando en controllers

--- myApp/database/config/config.js
+++ myApp/database/config/config.js
@@ -1,24 +1,24 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "root",
-    "database": "proyectoFinalProg2",
+    "password": null,
+    "database": "localHost",
     "host": "127.0.0.1",
     "dialect": "mysql",
     "operatorAliases": 0
   },
   "test": {
     "username": "root",
-    "password": "root",
-    "database": "proyectoFinalProg2",
+    "password": null,
+    "database": "localHost",
     "host": "127.0.0.1",
     "dialect": "mysql",
     "operatorAliases": 0
   },
   "production": {
     "username": "root",
-    "password": "root",
-    "database": "proyectoFinalProg2",
+    "password": null,
+    "database": "localHost",
     "host": "127.0.0.1",
     "dialect": "mysql",
     "operatorAliases": 0
