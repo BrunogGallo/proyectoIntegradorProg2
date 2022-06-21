5fd4a31a67937d14e9db8047475fae7d85d80bf3
marucalliera <89428723+marucalliera@users.noreply.github.com>
2022-06-21 19:55:14 -0300

    siguen cambios

--- myApp/routes/users.js
+++ myApp/routes/users.js
@@ -1,16 +1,41 @@
 //Importamos cheeck
 const express = require('express');
 const router = express.Router();
-
 const userController = require('../controllers/userController');
 
-router.get('/profile', userController.login)
+/* Requerir modulos de multer y path */
+let multer = require('multer');
+let path = require('path');
 
-router.post ('/edit', userController.procesarLogin)
+/* Configurar multer */
+
+let storage = multer.diskStorage({
+    destination : function(req, file, cb) {
+        cb(null, path.join(__dirname, '../public/images/usuarios'))
+    },
+    filename : function(req, file, cb) {
+      
+        /*          name campoDelForm          -   26052022                 .png  */
+        cb(null, file.fieldname + '-' + Date.now()+ path.extname(file.originalname))
+    }
+});
+
+let upload = multer({ storage : storage})
+
+/*Get users listing*/
+router.get ('/register', userController.register)
+
+router.post('/register', upload.single('fotoPerfil') , userController.procesarRegister);
+
+router.get('/login', userController.login)
+
 
 router.post ('/register', userController.procesarRegister)
 
-router.get ('/register', userController.register)
+router.post ('/edit', userController.procesarLogin)
+
+router.get('/logout', userController.logout)
+
 
 
 module.exports = router;
