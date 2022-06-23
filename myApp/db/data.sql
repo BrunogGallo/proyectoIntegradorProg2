46a0faf8fb242df7c90cb1b6a989a86c4a4e88ce
BrunogGallo <brunogiorgiogallo@mail.com>
2022-06-23 20:11:38 -0300

    completo datos para las tablas

--- myApp/db/data.sql
+++ myApp/db/data.sql
@@ -52,31 +52,34 @@ idSeguido		INT     UNSIGNED 	NOT NULL,
 FOREIGN KEY (idSeguidor) REFERENCES usuarios (id),
 FOREIGN KEY (idSeguido) REFERENCES usuarios (id)
 );
-//RangeError: Maximum call stack size exceeded//
 
-datos para usuario
-INSERT INTO usuarios VALUES (default, default, default, 'Juan', 'Perez', 'juanperez@gmail.com', 'juanchoperez', 'root', '2020-05-09', 0, 'https://upload.wikimedia.org/wikipedia/commons/c/c1/Lionel_Messi_20180626.jpg');
+///// DATOS PARA INSERTAR EN LAS TABLAS /////
 
-INSERT INTO usuarios VALUES (default, default, default, 'Fede', 'Fernandez', 'fedefdez@gmail.com', 'fedoferno', 'root', '2002-05-09', 0, 'https://upload.wikimedia.org/wikipedia/commons/c/c1/Lionel_Messi_20180626.jpg');
 
-INSERT INTO usuarios VALUES (default, default, default, 'Leo', 'Messi', 'leomessi@gmail.com', 'leomessi10', 'root', '1987-06-24', 0, 'https://upload.wikimedia.org/wikipedia/commons/c/c1/Lionel_Messi_20180626.jpg')
+DATOS USUARIOS
+INSERT INTO usuarios (`id`,`createdAt`,`updatedAt`,`nombre`,`apellido`,`email`,`nombreUsuario`,`contraseña`,`fechaNacimiento`,`seguidores`,`fotoPerfil`) VALUES (1,NULL,'2022-06-21','Leopoldo','Messi  ','juanperez@gmail.com','LeopoldoM12','root','2020-05-09',0,'fotoPerfil-1655849217827.jpeg');
+INSERT INTO usuarios (`id`,`createdAt`,`updatedAt`,`nombre`,`apellido`,`email`,`nombreUsuario`,`contraseña`,`fechaNacimiento`,`seguidores`,`fotoPerfil`) VALUES (2,NULL,'2022-06-21','Federico','Fernandez','fedefdez@gmail.com','fedoferno','root','2002-05-09',0,'fotoPerfil-1655848564896.jpeg');
+INSERT INTO usuarios (`id`,`createdAt`,`updatedAt`,`nombre`,`apellido`,`email`,`nombreUsuario`,`contraseña`,`fechaNacimiento`,`seguidores`,`fotoPerfil`) VALUES (3,NULL,'2022-06-22','Leo','Messi','leomessi@gmail.com','leomessi10','root','1987-06-24',0,'fotoPerfil-1655924213784.jpeg');
+INSERT INTO usuarios (`id`,`createdAt`,`updatedAt`,`nombre`,`apellido`,`email`,`nombreUsuario`,`contraseña`,`fechaNacimiento`,`seguidores`,`fotoPerfil`) VALUES (4,'2022-06-22','2022-06-22','Bruno','Gallo','','kunaguero','$2a$10$MBbYa5ilB0WjpyYMAu08qu7h5hyaASSGPjfAFrnR3fEsPdjEwzsSe','2022-06-01',NULL,'fotoPerfil-1655920346816.jpeg');
+INSERT INTO usuarios (`id`,`createdAt`,`updatedAt`,`nombre`,`apellido`,`email`,`nombreUsuario`,`contraseña`,`fechaNacimiento`,`seguidores`,`fotoPerfil`) VALUES (5,'2022-06-22','2022-06-22','Bruno','Gallo','brunogiorgiogallo@gmail.com','Brunoggallo','$2a$10$lDeB1svwo4aQp2edkzd02e0Y0ygYlwAatLNcNuOl2NxFSk4GwYMFi','2022-06-08',NULL,'fotoPerfil-1655920399633.jpeg');
+INSERT INTO usuarios (`id`,`createdAt`,`updatedAt`,`nombre`,`apellido`,`email`,`nombreUsuario`,`contraseña`,`fechaNacimiento`,`seguidores`,`fotoPerfil`) VALUES (6,'2022-06-22','2022-06-22','Bruno','Gallo','bgallo@gmail.com','bggallo','$2a$10$JPrHkAY6ICoA6kV2Vl6la.Be0e/YkBfRzkQulXiJyJsLW8mpwND5u','2022-06-21',NULL,'fotoPerfil-1655927164785.jpeg');
+INSERT INTO usuarios (`id`,`createdAt`,`updatedAt`,`nombre`,`apellido`,`email`,`nombreUsuario`,`contraseña`,`fechaNacimiento`,`seguidores`,`fotoPerfil`) VALUES (7,'2022-06-22','2022-06-23','Franco','martinez','fmartinez@gmail.com','fmartinez07','$2a$10$3gFQ44887X4zAJ504vpJEOZOqaLnBdkv8oxIc4Lu0db1GIsxQGx4i','2022-05-05',NULL,'fotoPerfil-1655948009967.webp');
+INSERT INTO usuarios (`id`,`createdAt`,`updatedAt`,`nombre`,`apellido`,`email`,`nombreUsuario`,`contraseña`,`fechaNacimiento`,`seguidores`,`fotoPerfil`) VALUES (8,'2022-06-23','2022-06-23','Wanchope','Abila','wanchoabila@gmail.com','wanchoabila9','$2a$10$KPcH4rcY8MlMbOTWyglWpe267xelKMfCh2XaNSMYAoTped9Kjr5zK','1993-01-05',NULL,'fotoPerfil-1655955625394.webp');
+INSERT INTO usuarios (`id`,`createdAt`,`updatedAt`,`nombre`,`apellido`,`email`,`nombreUsuario`,`contraseña`,`fechaNacimiento`,`seguidores`,`fotoPerfil`) VALUES (9,'2022-06-23','2022-06-23','Juan Roman','Riquelme','juanroriquelme@gmail.com','juanrr10','$2a$10$JQ8Csnbw..m/WFwJlTikLOSsEoBft9FwyyAGVfIDCxxd3fj2MX2NG','2022-06-18',NULL,'fotoPerfil-1656021058911.jpeg');
 
-datos para productos
-INSERT INTO productos VALUES (default, '2022-06-18', default, 'Chevrolet Onix', 'auto blanco moderno', 'https://imgcla.lavoz.com.ar/files/imagecache/ficha_aviso_mobile_462_253/avisos/aviso_auto/aviso-auto-chevrolet-onix-23058412-0302-48f4-b4bb-84c817f6346b-682.png', '1');
+DATOS PRODUCTOS
+INSERT INTO productos (`id`,`createdAt`,`updatedAt`,`nombreProducto`,`descripcion`,`imagen`,`idUsuario`) VALUES (7,'2022-06-23','2022-06-23','Corsa','auto viejo blanco, clasico','imagen-1656019519658.jpg',7);
+INSERT INTO productos (`id`,`createdAt`,`updatedAt`,`nombreProducto`,`descripcion`,`imagen`,`idUsuario`) VALUES (9,'2022-06-23','2022-06-23','Ferrari Enzo','Ferrari Enzo (no estoy para nada seguro que sea una enzo) moderno blanco','imagen-1655955847304.jpeg',8);
+INSERT INTO productos (`id`,`createdAt`,`updatedAt`,`nombreProducto`,`descripcion`,`imagen`,`idUsuario`) VALUES (13,'2022-06-23','2022-06-23','Jeep Classic','Jeep clasica negra, 0km','imagen-1655956523537.webp',8);
+INSERT INTO productos (`id`,`createdAt`,`updatedAt`,`nombreProducto`,`descripcion`,`imagen`,`idUsuario`) VALUES (15,'2022-06-23','2022-06-23','Lamborghini Murcielago','Lamborghini Murcielago (nuevamente, no se si estoy en lo correcto con el modelo), negro y moderno','imagen-1656020225134.jpeg',7);
+INSERT INTO productos (`id`,`createdAt`,`updatedAt`,`nombreProducto`,`descripcion`,`imagen`,`idUsuario`) VALUES (16,'2022-06-23','2022-06-23','Dodge RAM','Dodge RAM roja, nueva y moderna','imagen-1656020241785.webp',7);
 
-INSERT INTO productos VALUES (default, '2022-06-18', default, 'Chevrolet Onix', 'auto negro moderno', 'https://www.chevrolet.com.ar/content/dam/chevrolet/mercosur/argentina/espanol/index/cars/2020-onix-premier/colorizer/01-images/julio-20/4-onix-premier-black-meet-kettle-met.png?imwidth=960', '1');
-
-INSERT INTO productos VALUES (default, '2022-06-18', default, 'Chevrolet Onix', 'auto marron moderno', 'https://imgcla.lavoz.com.ar/files/imagecache/ficha_aviso_mobile_462_253/avisos/aviso_auto/aviso-auto--8572929.JPG	', '1');
-
-INSERT INTO productos VALUES (default, '2022-06-18', default, 'Chevrolet Onix', 'auto rojo moderno', 'https://www.megautos.com/wp-content/uploads/2019/09/nuevo-chevrolet-onix-2020-rojo-h1.jpg', '2');
-
-INSERT INTO productos VALUES (default, '2022-06-18', default, 'Chevrolet Onix', 'auto azul moderno', 'https://acroadtrip.blob.core.windows.net/catalogo-imagenes/l/RT_V_ec2a039ef81144019c35c9f6347dd121.jpg', '2')
-
-datos para comentarios
-INSERT INTO comentarios VALUES (default, default, 1, 1, 'muy recomendable');
-
-INSERT INTO comentarios VALUES (default, default, 2, 1, 'muy bueno');
-
-INSERT INTO comentarios VALUES (default, default, 2, 2, 'gran precio');
-
-INSERT INTO comentarios VALUES (default, default, 3, 1, 'muy recomendable')     
+DATOS COMENTARIOS
+INSERT INTO comentarios (`id`,`createdAt`,`idUsuario`,`idProducto`,`texto`) VALUES (5,NULL,7,9,'muy buen precio');
+INSERT INTO comentarios (`id`,`createdAt`,`idUsuario`,`idProducto`,`texto`) VALUES (7,NULL,8,7,'Un clasico');
+INSERT INTO comentarios (`id`,`createdAt`,`idUsuario`,`idProducto`,`texto`) VALUES (8,NULL,7,7,'Viste wancho, terrible');
+INSERT INTO comentarios (`id`,`createdAt`,`idUsuario`,`idProducto`,`texto`) VALUES (9,NULL,7,13,'Que buena foto!');
+INSERT INTO comentarios (`id`,`createdAt`,`idUsuario`,`idProducto`,`texto`) VALUES (11,NULL,8,15,'Increible!');
+INSERT INTO comentarios (`id`,`createdAt`,`idUsuario`,`idProducto`,`texto`) VALUES (12,NULL,8,15,'Precio?');
+INSERT INTO comentarios (`id`,`createdAt`,`idUsuario`,`idProducto`,`texto`) VALUES (13,NULL,8,16,'Que buena foto!');
+INSERT INTO comentarios (`id`,`createdAt`,`idUsuario`,`idProducto`,`texto`) VALUES (14,NULL,9,9,'Tremendo');
